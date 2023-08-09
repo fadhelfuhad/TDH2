5260a7b41db3f51ce18e9e5f7a48d0921bd2b89e
fathima-anam <fathimanam20@gmail.com>
2023-08-09 22:07:01 +0300

    Initial commit

---
+++ app/modal.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+import MapView from 'react-native-maps';
+
+export default function ModalScreen() {
+  return (
+    <View style={styles.container}>
+    <MapView style={styles.map} />
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
